07e58711c436ba117ff3d0ff8e9ddd0e52746630
Nikita Stavitskiy <n.saratovtsev@mail.ru>
2024-05-29 19:20:22 +0500

    Refactoring code

--- GeometryApp/MainApp.cs
+++ GeometryApp/MainApp.cs
@@ -1,92 +1,91 @@
 using GeometricAreaLibrary.Figure;
 
-namespace GeometryApp
+namespace GeometryApp;
+
+public class MainApp
 {
-    public class MainApp
+    public void Start()
     {
-        public void Start()
+        Console.WriteLine("Welcome new user");
+
+        while (true)
         {
-            Console.WriteLine("Welcome new user");
+            Console.WriteLine("Choose the following actions:" +
+                              "\n1-Area of a circle" +
+                              "\n2-Area of a triangle" +
+                              "\n3-Exit");
+            var answer = Console.ReadLine();
 
-            while (true)
+            if (!int.TryParse(answer, out var choose) || choose < 1 || choose > 3)
             {
-                Console.WriteLine("Choose the following actions:" +
-                                  "\n1-Area of a circle" +
-                                  "\n2-Area of a triangle" +
-                                  "\n3-Exit");
-                var answer = Console.ReadLine();
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+                continue;
+            }
 
-                if (!int.TryParse(answer, out var choose) || choose < 1 || choose > 3)
-                {
-                    Console.WriteLine("Invalid input. Please enter a valid number.");
-                    continue;
-                }
+            if (choose == 3)
+                break;
 
-                if (choose == 3)
+            switch (choose)
+            {
+                case 1:
+                    CircleArea();
                     break;
-
-                switch (choose)
-                {
-                    case 1:
-                        CircleArea();
-                        break;
-                    case 2:
-                        TriangleArea();
-                        break;
-                }
+                case 2:
+                    TriangleArea();
+                    break;
             }
         }
+    }
 
-        private void CircleArea()
+    private void CircleArea()
+    {
+        Console.WriteLine("Enter the radius of the circle");
+        if (!double.TryParse(Console.ReadLine(), out var circleRadius) || circleRadius <= 0)
         {
-            Console.WriteLine("Enter the radius of the circle");
-            if (!double.TryParse(Console.ReadLine(), out var circleRadius) || circleRadius <= 0)
-            {
-                Console.WriteLine("Invalid input. Please enter a valid number greater than zero.");
-                return;
-            }
-
-            var circle = new Circle(circleRadius);
-            var answer = circle.CalculateArea();
-            Console.WriteLine($"Area of a circle: {answer}");
+            Console.WriteLine("Invalid input. Please enter a valid number greater than zero.");
+            return;
         }
 
-        private void TriangleArea()
+        var circle = new Circle(circleRadius);
+        var answer = circle.CalculateArea();
+        Console.WriteLine($"Area of a circle: {answer}");
+    }
+
+    private void TriangleArea()
+    {
+        Console.WriteLine("Enter first side");
+        if (!double.TryParse(Console.ReadLine(), out var aSideTriangle) || aSideTriangle <= 0)
         {
-            Console.WriteLine("Enter first side");
-            if (!double.TryParse(Console.ReadLine(), out var aSideTriangle) || aSideTriangle <= 0)
-            {
-                Console.WriteLine("Invalid input. Please enter a valid number greater than zero.");
-                return;
-            }
-
-            Console.WriteLine("Enter second side");
-            if (!double.TryParse(Console.ReadLine(), out var bSideTriangle) || bSideTriangle <= 0)
-            {
-                Console.WriteLine("Invalid input. Please enter a valid number greater than zero.");
-                return;
-            }
-
-            Console.WriteLine("Enter third side");
-            if (!double.TryParse(Console.ReadLine(), out var cSideTriangle) || cSideTriangle <= 0)
-            {
-                Console.WriteLine("Invalid input. Please enter a valid number greater than zero.");
-                return;
-            }
-
-            Console.WriteLine("Enter the radius of the triangle");
-            if (!double.TryParse(Console.ReadLine(), out var triangleRadius) || triangleRadius <= 0)
-            {
-                Console.WriteLine("Invalid input. Please enter a valid number greater than zero.");
-                return;
-            }
-
-            var triangle = new Triangle(aSideTriangle, bSideTriangle, cSideTriangle, triangleRadius);
-            var answer = triangle.CalculateArea();
-            var isRightTriangle = triangle.IsRightTriangle();
-            
-            Console.WriteLine(isRightTriangle ? "Triangle is rectangular" : "Triangle is not rectangular");
-            Console.WriteLine($"Area of a triangle: {answer}");
+            Console.WriteLine("Invalid input. Please enter a valid number greater than zero.");
+            return;
         }
+
+        Console.WriteLine("Enter second side");
+        if (!double.TryParse(Console.ReadLine(), out var bSideTriangle) || bSideTriangle <= 0)
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number greater than zero.");
+            return;
+        }
+
+        Console.WriteLine("Enter third side");
+        if (!double.TryParse(Console.ReadLine(), out var cSideTriangle) || cSideTriangle <= 0)
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number greater than zero.");
+            return;
+        }
+
+        Console.WriteLine("Enter the radius of the triangle");
+        if (!double.TryParse(Console.ReadLine(), out var triangleRadius) || triangleRadius <= 0)
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number greater than zero.");
+            return;
+        }
+
+        var triangle = new Triangle(aSideTriangle, bSideTriangle, cSideTriangle, triangleRadius);
+        var answer = triangle.CalculateArea();
+        var isRightTriangle = triangle.IsRightTriangle();
+
+        Console.WriteLine(isRightTriangle ? "Triangle is rectangular" : "Triangle is not rectangular");
+        Console.WriteLine($"Area of a triangle: {answer}");
     }
 }
